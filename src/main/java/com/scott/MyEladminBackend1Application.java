c49ed3ffde80b1c557b1d863767b1af9297675c8
liscott2020 <li.scott@outlook.com>
2022-12-28 16:36:11 +0800

    1. 初始化项目

---
+++ src/main/java/com/scott/MyEladminBackend1Application.java
@@ -0,0 +1,13 @@
+package com.scott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEladminBackend1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyEladminBackend1Application.class, args);
+    }
+
+}
